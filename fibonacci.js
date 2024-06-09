d505a2757c2dcd0e0661b065d2570db6aa07eeea
josephl17 <joseph.lauria17@gmail.com>
2024-06-08 23:52:27 -0500

    added solution in JS

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-  return 0;
+  // base cases
+  if (num === 0) {
+    return 0
+  }
+
+  else if (num == 1) {
+    return 1
+  }
+  // recursive case
+  else {
+    return fibonacci(num -1) + fibonacci(num -2)
+  }
 }
+
+console.log((fibonacci(0) == 0))
+console.log((fibonacci(2) == 1))
+console.log((fibonacci(5) == 5))
+console.log((fibonacci(8) == 21))
+console.log((fibonacci(11) == 89))
 
 module.exports = fibonacci;
